8b73ed7e77ecc1f4db2df0c452c731512cad418d
AhmedTarek10 <ahmedtarekessa10@gmail.com>
2025-07-20 17:03:23 +0300

    OrdersSet

---
+++ src/app/features/user/components/orders/user-no-orders/user-no-orders.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-no-orders',
+  imports: [],
+  templateUrl: './user-no-orders.html',
+  styleUrl: './user-no-orders.css'
+})
+export class UserNoOrders {
+
+}
