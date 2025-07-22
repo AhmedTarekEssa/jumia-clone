f44284add267d6b0775f490d98920f3480179126
mariamkashkoush26@gmail.com <mariamkashkoush26@gmail.com>
2025-07-22 12:28:36 +0300

    authentication

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarMain } from "./shared/components/navbars/navbar-main/navbar-main";
 import { HomeContainer } from "./features/home/home-container/home-container";
+import AuthService from './core/services/auth';
+import { Login } from "./features/auth/components/login/login";
+import { CheckEmail } from "./features/auth/components/check-email/check-email";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarMain, HomeContainer],
+  imports: [RouterOutlet, NavbarMain, HomeContainer, Login, CheckEmail],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'jumia-clone';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    //when user refreshes the page , the cookie is read and user is logged in automatically
+    this.authService.initUserFromCookie();
+  }
+
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+
+    production: false,
+    apiUrl: 'http://localhost:5087/api',
+    authRoutes: {
+        login: '/auth/login',
+        register: '/auth/register',
+        checkEmail: '/Auth/email-check',
+        verifyOtp: '/auth/verify-otp',
+        logout: '/auth/logout',   
+    }
+};
