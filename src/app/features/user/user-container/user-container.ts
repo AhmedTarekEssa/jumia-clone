25a0c6168777b17862f1fa423906ab348a906042
AhmedTarek10 <ahmedtarekessa10@gmail.com>
2025-07-22 13:29:56 +0300

    order front end done

--- src/app/features/user/user-container/user-container.ts
+++ src/app/features/user/user-container/user-container.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-container',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './user-container.html',
   styleUrl: './user-container.css'
 })
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -40,7 +40,15 @@ export const environment = {
     GetMainCategories: "Categories/main",
 
     GetAttributes: (parentId: number) => `Categories/${parentId}/attributes`,
-  }
+  },
+  Orders: {
+    GetAll: "Order",
+    GetById: (id: number) => `Order/get-by-id/${id}`,
+    Create: "Order",
+    UpdateStatus: (id: number) => `Order/${id}/status`,
+    GetByUserId: (userId: string) => `Order/customer/${userId}`,
+    getCurrentUserOrders: "Order/current-customer"
+  },
 
 };
 
