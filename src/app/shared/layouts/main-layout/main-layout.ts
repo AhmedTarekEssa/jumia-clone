2ec324ef931e807bff11501d3c08ed0d3c1887a1
AhmedTarek10 <ahmedtarekessa10@gmail.com>
2025-07-24 23:51:56 +0300

    live chat added

--- src/app/shared/layouts/main-layout/main-layout.ts
+++ src/app/shared/layouts/main-layout/main-layout.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FooterMain } from "../../components/footers/footer-main/footer-main";
 import { NavbarMain } from "../../components/navbars/navbar-main/navbar-main";
 import { RouterOutlet } from '@angular/router';
+import { LiveChat } from "../../components/live-chat/live-chat";
+import { CookieService } from 'ngx-cookie-service';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-main-layout',
-  imports: [FooterMain, NavbarMain , RouterOutlet],
+  imports: [FooterMain, NavbarMain, RouterOutlet, LiveChat, CommonModule],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
 })
-export class MainLayout {
+export class MainLayout implements OnInit {
+  showLiveChat: boolean = false;
+  constructor(
+    private cookies: CookieService,
+    private cdr: ChangeDetectorRef
+  ) {
+    }
+  ngOnInit(): void {
+    if (this.cookies.check('UserInfo')&& this.cookies.get('UserInfo') !== null) {
+      this.showLiveChat = true;
+      this.cdr.detectChanges();
+    }
+
+  }
 
 }
