a36fda1eb21b830872c11dd46b5a1766484b64c0
joeshaker <youssefshaker296@gmail.com>
2025-07-28 19:13:30 +0300

    ProductReview

--- src/app/features/products/product.routes.ts
+++ src/app/features/products/product.routes.ts
@@ -4,18 +4,18 @@ import { NgModule } from '@angular/core';
 import { ProductReviewShowAll } from './components/product-review-show-all/product-review-show-all';
 
 export const routes: Routes = [
-  {
-    path: 'Products/:id',
-    loadComponent: () => import('./components/product-detail/product-detail').then(m => m.ProductDetailC),
-  },
-  {
-    path:'ProductsReviews/:id',
-    loadComponent: () => import('./components/product-review-show-all/product-review-show-all').then(m => m.ProductReviewShowAll),
-  },
-  {
-    path:'',
-    redirectTo: 'Products/16',
-    pathMatch: 'full'
-  }
+  // {
+  //   path: 'Products/:id',
+  //   loadComponent: () => import('./components/product-detail/product-detail').then(m => m.ProductDetailC),
+  // },
+  // {
+  //   path:'ProductsReviews/:id',
+  //   loadComponent: () => import('./components/product-review-show-all/product-review-show-all').then(m => m.ProductReviewShowAll),
+  // },
+  // {
+  //   path:'',
+  //   redirectTo: 'Products/16',
+  //   pathMatch: 'full'
+  // }
 ];
 
